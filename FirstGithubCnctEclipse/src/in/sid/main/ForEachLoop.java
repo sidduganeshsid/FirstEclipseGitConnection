46c0f7df3377de201a670091ea4a5dca794d481a
sidduganeshsid <sidduganeshengineer@gmail.com>
2024-01-01 15:02:11 +0530

    foreach loop added

---
+++ FirstGithubCnctEclipse/src/in/sid/main/ForEachLoop.java
@@ -0,0 +1,14 @@
+package in.sid.main;
+
+public class ForEachLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {23,5,3,4};
+		for(int elem : arr)
+		{
+			System.out.println(elem);		}
+
+	}
+
+}
